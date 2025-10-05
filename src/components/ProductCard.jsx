b35b9fc2e622ec4294b9e47bf3cd15f00501ffa2
amletodellaloggia <amletodellaloggia@gmail.com>
2025-10-05 17:39:47 +0200

    🐞 fix:

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 
 const ProductCard = ({ product }) => (
   <div className="product-card card shadow-sm">
-<img src={`/images/${product.image_url}`} alt={product.name} className="product-card-img card-img-top" />
+    <img
+      src={`http://localhost:3000/images/${product.image_url}`}
+      alt={product.name}
+      className="product-card-img card-img-top"
+    />
     <div className="product-card-body card-body d-flex flex-column">
       <h5 className="product-card-title card-title">{product.name}</h5>
       <p className="product-card-price card-text">{product.price} €</p>
-      <Link to={`/products/${product.product_id}`} className="btn btn-outline-primary product-card-btn">Details</Link>
+      <Link to={`/products/${product.product_id}`} className="btn btn-outline-primary product-card-btn">
+        Details
+      </Link>
     </div>
   </div>
 );
